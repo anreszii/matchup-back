dc6b3b370336dff2524a27759d2184dbb9d74c31
PerceptControl <sa1rac.work@gmail.com>
2022-07-08 17:07:52 +0400

    create working server

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express')
-
 const fileUploader = require('express-fileupload')
 
+const mongoose = require('mongoose')
 const app = express()
 
 app.use(fileUploader())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+mongoose.connect(
+  'mongodb+srv://Perception:eWvXO4EXlwTQ2Dpx@testcluster.vbwobca.mongodb.net/?retryWrites=true&w=majority',
+  (error) => {
+    if (error) return console.log(error)
+    console.log('Connected to database')
+  },
+)
+
+require('./models/User')
 app.use(require('./routes'))
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`)
+})
