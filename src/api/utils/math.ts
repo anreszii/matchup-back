bca82e7ac4275bdcd125e11bf2eb29cc84bdb892
PerceptControl <sa1rac.work@gmail.com>
2022-07-27 13:41:12 +0400

    update path

--- src/api/utils/math.ts
+++
@@ -1,8 +0,0 @@
-export function minMax(min: number, max: number) {
-  let rand = min + Math.random() * (max + 1 - min)
-  return Math.floor(rand)
-}
-
-export function getRounded(num: number, round: number) {
-  return Math.round(num / round) * round
-}
