bd480d6053370dda87a2b8dcfa60c367ff11206a
PerceptControl <sa1rac.work@gmail.com>
2022-08-13 19:04:46 +0300

    added TODO

---
+++ src/Models/MatchMaking/MatchList.ts
@@ -0,0 +1,35 @@
+import {
+  prop,
+  getModelForClass,
+  Ref,
+  ReturnModelType,
+} from '@typegoose/typegoose'
+import * as mongoose from 'mongoose'
+import { SUPPORTED_GAMES } from '../..'
+import { Member } from './Member'
+
+/**
+ * @TODO
+ * Add match score
+ */
+class MatchList {
+  @prop({ required: true, unique: true })
+  public id!: string
+  @prop({ required: true })
+  public game!: SUPPORTED_GAMES
+  @prop({ required: true, ref: Member })
+  public members: Ref<Member>
+
+  public static async findByID(
+    this: ReturnModelType<typeof MatchList>,
+    id: string,
+  ) {
+    return this.findOne({ id })
+  }
+
+  public static async getAll(this: ReturnModelType<typeof MatchList>) {
+    return this.find()
+  }
+}
+
+export const MatchListModel = getModelForClass(MatchList)
