0d935747d713a0e12453b14e57fd7abcd5b1a371
PerceptControl <sa1rac.work@gmail.com>
2022-09-14 17:44:17 +0300

    добавлено: импорт обработчиков guild

--- src/API/Sockets/Handlers/index.ts
+++ src/API/Sockets/Handlers/index.ts
@@ -2,7 +2,8 @@
  * Файл существует только для документации.
  * Не использовать import на него, т.к. компиляция tsc выдаст undefined для всех обработчиков
  */
-export * from './admin'
 export * from './basic'
-export * from './match'
+export * from './admin'
 export * from './user'
+export * from './guild'
+export * from './match'
