6f0f67ef749980014f1701640bf5cac61eb4c4c7
PerceptControl <sa1rac.work@gmail.com>
2022-09-20 15:19:51 +0300

    добавлено: автоматическое удаление неиспользуемых каналов

--- src/discordBot.ts
+++ src/discordBot.ts
@@ -1,4 +1,5 @@
 require('dotenv').config()
+import type { Collection, Guild, GuildMember } from 'discord.js'
 import { VoiceState } from 'discord.js'
 import { guardCommandVoice } from './API/Discord/commandVoiceChatGuard'
 import { distribute } from './API/Discord/distributor'
@@ -7,6 +8,14 @@ import { StateManager } from './Classes/Discord/StateManager'
 
 export function init(token: string) {
   let dsClient = new DiscordClient(token)
+  setInterval(function () {
+    dsClient.guilds.then(async (guilds) => {
+      for (let [_, guild] of guilds) {
+        markChannelsForDelete(await guild.fetch())
+      }
+    })
+  }, 1000 * 60 * 15)
+
   dsClient.client.on(
     'voiceStateUpdate',
     async (_: unknown, newState: VoiceState) => {
@@ -17,4 +26,27 @@ export function init(token: string) {
       }
     },
   )
+}
+
+function markChannelsForDelete(guild: Guild) {
+  for (let [id, channel] of guild.channels.cache) {
+    if (!channel.name.startsWith('command') || !channel.isVoiceBased) continue
+    let members = channel.members as Collection<string, GuildMember>
+    let func = checkChannelForDelete.bind({
+      guild: guild,
+      id: id,
+    })
+    if (members.size < 1) setTimeout(func, 1000 * 60 * 5)
+  }
+}
+
+function checkChannelForDelete(this: { guild: Guild; id: string }) {
+  if (!this.guild.channels.cache.has(this.id)) return
+  this.guild.channels.cache
+    .get(this.id)!
+    .fetch()
+    .then(async (channel) => {
+      let members = channel.members as Collection<string, GuildMember>
+      if (members.size < 1) await channel.delete()
+    })
 }
