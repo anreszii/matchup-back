16019fdec5b52390613a1fb6361318316d25a421
PerceptControl <sa1rac.work@gmail.com>
2022-09-26 15:07:09 +0300

    изменено: убран REWARD_TYPE

--- src/Interfaces/index.d.ts
+++ src/Interfaces/index.d.ts
@@ -2,6 +2,5 @@ export * from './Manager'
 export * from './MatchMaking'
 export * from './Chat'
 
-export declare type REWARD_TYPE = 'exp' | 'mp'
 export declare type USER_ROLE = 'default' | 'privileged' | 'admin'
 export declare type USER_PREFIX = 'YouTube'
