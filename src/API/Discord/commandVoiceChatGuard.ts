3b5ededd149a24b46f45f91438fffcce77105bfd
PerceptControl <sa1rac.work@gmail.com>
2022-09-26 15:22:39 +0300

    исправлено: фикс защиты голосового чата

--- src/API/Discord/commandVoiceChatGuard.ts
+++ src/API/Discord/commandVoiceChatGuard.ts
@@ -3,7 +3,10 @@ import type { StateManager } from '../../Classes/Discord/StateManager'
 export function guardCommandVoice(manager: StateManager) {
   let channelCommand = manager.channelCommand
   let memberCommand = manager.memberCommand
-  if (!memberCommand) return
+  if (!memberCommand) {
+    manager.channel = null
+    return
+  }
 
   if (channelCommand != memberCommand) {
     manager.channel = null
