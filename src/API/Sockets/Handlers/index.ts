69926f9f9eca9e116b31925c99753a5e4372d8f7
PerceptControl <sa1rac.work@gmail.com>
2022-10-09 14:14:35 +0300

    удалено: отдельные обработчики для админа

--- src/API/Sockets/Handlers/index.ts
+++ src/API/Sockets/Handlers/index.ts
@@ -3,7 +3,6 @@
  * Не использовать import на него, т.к. компиляция tsc выдаст undefined для всех обработчиков
  */
 export * from './basic'
-export * from './admin'
 export * from './user'
 export * from './guild'
 export * from './match'
