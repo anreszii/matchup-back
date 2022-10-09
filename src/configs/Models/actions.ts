ecccbf5a52ff1b1822df37d5300ebbc875bd5be1
PerceptControl <sa1rac.work@gmail.com>
2022-10-09 14:16:52 +0300

    добавлено: TODO

--- src/configs/Models/actions.ts
+++ src/configs/Models/actions.ts
@@ -15,6 +15,7 @@ export type MODELS_ACTION_LIST =
   | 'MatchList/setScreen'
   | 'ReportList/addProof'
 
+/** @TODO разделить менеджеры на разные модели, чтобы можно было назначать для каждой модели свои роли(например для гильдии) */
 export const MODELS_ACTIONS: Map<MODELS_ACTION_LIST, number> = new Map()
 MODELS_ACTIONS.set('User/checkLevel', 0)
 MODELS_ACTIONS.set('User/addRelation', 0)
@@ -29,7 +30,7 @@ MODELS_ACTIONS.set('Guild/changeGuildName', 0)
 MODELS_ACTIONS.set('Guild/changeTagName', 0)
 MODELS_ACTIONS.set('Guild/join', 0)
 MODELS_ACTIONS.set('Guild/leave', 0)
-MODELS_ACTIONS.set('MatchList/setScreen', 0)
+MODELS_ACTIONS.set('MatchList/setScreen', 2)
 MODELS_ACTIONS.set('ReportList/addProof', 0)
 
 export function isValidModelAction(
