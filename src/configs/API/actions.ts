b59d979171ce09ab9ab30c39284d0f4ae9838611
PerceptControl <sa1rac.work@gmail.com>
2022-11-08 14:11:05 +0300

    изменено: добавлен пропущенный контроллер

--- src/configs/API/actions.ts
+++ src/configs/API/actions.ts
@@ -1,16 +1,16 @@
 export type API_ACTION_LIST =
-  | 'find_lobby'
-  | 'leave_lobby'
-  | 'invite_to_lobby'
-  | 'join_to_lobby'
-  | 'sync_lobby'
-  | 'get_lobby_players_count'
   | 'get_lobby_count'
+  | 'find_lobby'
+  | 'join_to_lobby'
+  | 'invite_to_lobby'
+  | 'leave_lobby'
+  | 'get_lobby_players_count'
   | 'get_ready'
-  | 'vote'
   | 'get_maps'
-  | 'change_command'
   | 'get_captain'
+  | 'vote'
+  | 'sync_lobby'
+  | 'change_command'
   | 'create_team'
   | 'join_team'
   | 'leave_team'
@@ -21,20 +21,21 @@ export type API_ACTION_LIST =
   | 'drop_relation'
 
 export const API_ACTIONS: Map<API_ACTION_LIST, number> = new Map()
-API_ACTIONS.set('find_lobby', 0)
-API_ACTIONS.set('leave_lobby', 0)
-API_ACTIONS.set('invite_to_lobby', 0)
-API_ACTIONS.set('vote', 0)
-API_ACTIONS.set('join_to_lobby', 0)
-API_ACTIONS.set('get_lobby_players_count', 0)
 API_ACTIONS.set('get_lobby_count', 0)
+API_ACTIONS.set('find_lobby', 0)
+API_ACTIONS.set('join_to_lobby', 0)
+API_ACTIONS.set('invite_to_lobby', 0)
+API_ACTIONS.set('leave_lobby', 0)
+API_ACTIONS.set('get_lobby_players_count', 0)
+API_ACTIONS.set('get_ready', 0)
+API_ACTIONS.set('get_maps', 0)
 API_ACTIONS.set('get_captain', 0)
+API_ACTIONS.set('vote', 0)
 API_ACTIONS.set('sync_lobby', 0)
 API_ACTIONS.set('create_team', 0)
 API_ACTIONS.set('join_team', 0)
 API_ACTIONS.set('leave_team', 0)
 API_ACTIONS.set('get_teams', 0)
-API_ACTIONS.set('get_maps', 0)
 API_ACTIONS.set('chat', 0)
 API_ACTIONS.set('add_relation', 0)
 API_ACTIONS.set('drop_relation', 0)
