c57788986233086b2b41640bf455f6e0f272b368
PerceptControl <sa1rac.work@gmail.com>
2022-11-24 18:17:16 +0300

    изменено: работа с изображениями перенесена в http

---
+++ src/API/HTTP/image.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express'
+import { TechnicalCause, TechnicalError } from '../../error'
+import { ImageModel } from '../../Models/Image'
+import { validateToken } from '../../Token/index'
+
+const router = Router()
+router.post('/upload', validateToken, async (req, res, next) => {
+  try {
+    let image = req.files?.image
+    if (!image || image instanceof Array)
+      throw new TechnicalError('image', TechnicalCause.INVALID_FORMAT)
+    let document = await ImageModel.create({
+      buffer: image.data,
+      mimeType: image.mimetype,
+    })
+
+    res.send(document._id)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.get('/:id', validateToken, async (req, res, next) => {
+  try {
+    let id = req.params.id
+    if (!id) throw new TechnicalError('id', TechnicalCause.REQUIRED)
+
+    let image = await ImageModel.findById(id)
+    if (!image) throw new TechnicalError('image', TechnicalCause.NOT_EXIST)
+    res.send(image.buffer.toString('base64url'))
+  } catch (e) {
+    next(e)
+  }
+})
+
+module.exports = router
