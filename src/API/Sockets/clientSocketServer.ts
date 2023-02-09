06735bca9854f40bc711b827d95a2719177a9ced
PerceptControl <sa1rac.work@gmail.com>
2023-02-09 15:51:16 +0300

    изменено: исправлен баг последовательности импортов

--- src/API/Sockets/clientSocketServer.ts
+++ src/API/Sockets/clientSocketServer.ts
@@ -11,11 +11,18 @@
 import { IDataEscort } from 'gamesocket.io'
 import { WS_SERVER } from '../../app'
 import { PLAYERS } from '../../Classes/MatchMaking/MemberManager'
-import { StandOff_Lobbies } from '.'
+import { Logger } from '../../Utils/Logger'
 
+const logger = new Logger('Web Socket Server', 'Close listener')
 export let clientServer = WS_SERVER.of(process.env.CLIENT_NAMESPACE!)
 clientServer.on('close', async (escort: IDataEscort) => {
+  const { StandOff_Lobbies } = require('.')
   let socket = escort.get('socket') as { [key: string]: any }
+  logger.info(
+    `[SOCKET ${socket.id}] LEAVED. CODE: ${escort.get(
+      'code',
+    )}. MESSAGE: ${escort.get('message')}`,
+  )
   if (typeof socket.username == 'string') {
     let player = await PLAYERS.get(socket.username)
     if (!player.lobbyID) return
