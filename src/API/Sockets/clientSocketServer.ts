27b4cff734c6c41f4610b3bb435a9974a7e44252
PerceptControl <sa1rac.work@gmail.com>
2023-02-09 13:17:32 +0300

    изменено: оптимизация выхода из лобби

--- src/API/Sockets/clientSocketServer.ts
+++ src/API/Sockets/clientSocketServer.ts
@@ -8,6 +8,21 @@
  * @packageDocumentation
  */
 
+import { IDataEscort } from 'gamesocket.io'
 import { WS_SERVER } from '../../app'
+import { PLAYERS } from '../../Classes/MatchMaking/MemberManager'
+import { StandOff_Lobbies } from '.'
 
 export let clientServer = WS_SERVER.of(process.env.CLIENT_NAMESPACE!)
+clientServer.on('close', async (escort: IDataEscort) => {
+  let socket = escort.get('socket') as { [key: string]: any }
+  if (typeof socket.username == 'string') {
+    let player = await PLAYERS.get(socket.username)
+    if (!player.lobbyID) return
+
+    let lobby = StandOff_Lobbies.get(player.lobbyID)
+    if (!lobby) return (player.lobbyID = undefined)
+
+    if (lobby.state == 'searching') await lobby.leave(player.name)
+  }
+})
